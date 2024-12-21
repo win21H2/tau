7af1861c22bdc868fb47f59a02825c3057c5bc89
win21H2 <1024Bitcoins@gmail.com>
2024-12-20 22:04:58 -0600

    Refactor control unit to use opcode for ALU operation and remove instruction register module; enhance top testbench with detailed comments on instruction execution phases

--- control_unit.v
+++ control_unit.v
@@ -1,3 +1,11 @@
+/*
+CU REFERENCE
+
+INSERTION/REMOVAL
+[OP 0000] - LOAD
+[OP 1111] - STORE
+*/
+
 module control_unit (
     input wire [3:0] opcode,
     output reg [3:0] alu_op,
@@ -11,24 +19,23 @@ module control_unit (
     always @(*) begin
         case(opcode)
             4'b0000: begin // load
-                alu_op = 4'b0000;
+                alu_op = opcode;
                 pc_load = 1'b0;
                 ir_load = 1'b1;
                 mem_read = 1'b1;
                 mem_write = 1'b0;
                 reg_write = 1'b1;
             end
-            4'b0001: begin // store
-                alu_op = 4'b0000;
+            4'b1111: begin // store
+                alu_op = opcode;
                 pc_load = 1'b0;
                 ir_load = 1'b1;
                 mem_read = 1'b0;
                 mem_write = 1'b1;
                 reg_write = 1'b0;
             end
-
             default: begin
-                alu_op = 4'b0000;
+                alu_op = opcode;
                 pc_load = 1'b0;
                 ir_load = 1'b1;
                 mem_read = 1'b0;
--- top_tb.v
+++ top_tb.v
@@ -1,3 +1,45 @@
+/*
+~~~ Example instruction ~~~
+addi x1, x2, 10
+Add constant "10" [00001010] to the contents of x2 at address [00010000 00000000 00000001] (using ALU opcode [0000])
+
+
+~~~ PC (program counter) ~~~
+Holds address of next instruction to execute (can either increment, jump to, or halt at an instruction)
+
+~~~ CU (control unit) ~~~
+Orchestreates all operations (such as when memory should be read/written) using an opcode
+
+~~~ ALU (arithmetic logic unit) ~~~
+Runs arithmetic or logical operations on data (using an opcode, that selects the type of operation)
+
+~~~ MAR (memory address register) ~~~
+A register that stores the address of the memory location (address) from which data is written to/read from (acts like an interface between the CPU and memory)
+
+~~~ MBR (memory buffer register) ~~~
+A temporary storage register that holds data being transferred to/from memory (acts like a buffer between the CPU and memory)
+
+~~~ IR (instruction register) ~~~
+A register that holds the current instruction being executed (holds the instruction until it's decoded and executed)
+
+
+~~~ Fetch Phase ~~~
+1) Address Transfer - The address for the instruction, held in the PC, is copied into the MAR
+2) Memory Read Command - The CU issues a read command to the memory, instructing it to retrieve the data at the address specified by the MAR
+3) Data Retrieval - The retrieved instruction is placed into the MBR
+4) Instruction Transfer - Finally, the instruction from the MBR is transferred to the IR for decoding (PC is also generally incremented)
+
+~~~ Decode Phase ~~~
+1) Opcode Analysis - The CU analyzes the opcode of the instruction stored in the IR to determine what operation needs to be performed
+2) Operand Identification - The instruction is broken down into its components, identifying which registers or constants are needed for execution
+3) Control Signal Generation - The CU sends control signals to fetch any necessary constants from memory (i.e. loading additional operands)
+
+~~~ Execute Phase ~~~
+1) ALU Operation - Once all necessary values are available, they are sent to the ALU along with the opcode indicating what operation should be performed (e.g., addition)
+2) Result Handling - Operation output is sent back to the designated register as specified by the instruction (either via direct transfer or MBR/MAR if interfacing with memory)
+3) Storage of Result - If needed, results can be stored back into RAM or another memory location based on further instructions or as part of subsequent operations
+*/
+
 module top_tb;
 	reg reset;
     reg clk_enable;
@@ -65,11 +107,8 @@ module top_tb;
         
         #10
 		clk_enable = 1;
-		
-		#10
-		lsi_enable = 1;
 
-		#100
+		#10
 
         $finish;
     end
